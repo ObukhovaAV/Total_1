aac5e638c33e7ad98fa113e3b866e5d870d5a82b
Anastasya <okayota83@mail.ru>
2022-11-26 14:16:27 +0400

    add Task

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+void SortArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=3)
+        Console.Write (array[i] + " ");
+        else
+        Console.Write (" ");
+    }
+}
+
+string [] array = {"1234", "2", "-2","computer science", "can", "do"};
+SortArray (array);
